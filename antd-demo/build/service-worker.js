ede00e1318f5500ccb708659528ca1e5d5832ac0
xuwenliu <1436731293@qq.com>
2019-04-12 16:21:52 +0700

    feat(add) 重新来

--- antd-demo/build/service-worker.js
+++ antd-demo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adafa1c82f89e296389c9fad91df266c.js"
+  "/react-antd/precache-manifest.789388e573249a04be7c20c04d0382f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-antd/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
