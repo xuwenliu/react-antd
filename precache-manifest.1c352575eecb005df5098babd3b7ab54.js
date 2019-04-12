38d71af9ea8d4f7914ea31618a61066609dee81d
xuwenliu <1436731293@qq.com>
2019-04-12 16:55:46 +0700

    Updates

--- precache-manifest.1c352575eecb005df5098babd3b7ab54.js
+++ precache-manifest.1c352575eecb005df5098babd3b7ab54.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-antd/static/js/runtime~main.a41404b4.js"
   },
   {
-    "revision": "5ac3f45e52279f76831d",
-    "url": "/react-antd/static/js/main.5ac3f45e.chunk.js"
+    "revision": "5d20310e52b3248893f1",
+    "url": "/react-antd/static/js/main.5d20310e.chunk.js"
   },
   {
     "revision": "6b0694817162579bd890",
     "url": "/react-antd/static/js/1.6b069481.chunk.js"
   },
   {
-    "revision": "5ac3f45e52279f76831d",
+    "revision": "5d20310e52b3248893f1",
     "url": "/react-antd/static/css/main.cd048e24.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-antd/static/css/1.452eb5e7.chunk.css"
   },
   {
-    "revision": "a80039d0b95f128628fd027b66fb0e2e",
+    "revision": "82e650bbf74e28ddc990eaada32f3fe2",
     "url": "/react-antd/index.html"
   }
 ];
