a091013169e8c70873b95a58af85f0a8346f86ed
xuwenliu <1436731293@qq.com>
2019-04-26 16:34:32 +0700

    feat(add) 轮播图不显示

--- antd-demo/build/service-worker.js
+++ antd-demo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-antd/precache-manifest.0bcd5de8b77983375320e2c32343d13f.js"
+  "/react-antd/precache-manifest.9b3aad8dd9cbbaf76028f0ee0efb6416.js"
 );
 
 workbox.clientsClaim();
